26f2e5a24ae5acd6dff82508ed34b2190943f70b
TOUFGA IBRAHIM <itoufga@leyton.com>
2023-08-07 22:00:24 +0100

    add docker-compose.yml to build and run the application init swagger

--- src/main/java/ma/sir/ged/config/SwaggerConfig.java
+++ src/main/java/ma/sir/ged/config/SwaggerConfig.java
@@ -1,22 +1,23 @@
 package ma.sir.ged.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfig {
-    /*
-    @Bean
-    public Docket Api() {
-        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.any()).build().apiInfo(getApiInfo());
-    }
 
-    public ApiInfo getApiInfo() {
-        return new ApiInfoBuilder().title("ged").version("2.0").description("generated ")
-        .license("he").build();
+    @Bean
+    public OpenAPI gedMinioOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ged-minio documentation")
+                        .version("1.0.0")
+                        .description("All the endpoints existing to manage minio operations")
+                );
     }
-    */
 
 }
